df195d03139d9a7b178a87f73eb3c6150f47944c
lobanov4real <lobanov4real@gmail.com>
2022-11-16 21:47:09 +0300

    add inventory file fo ansible

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,10 +3,8 @@
 sudo add-apt-repository ppa:deadsnakes/ppa
 apt-get update
 apt-get install -y python3-pip ansible libssl-dev rustc libffi-dev git
-python3 -m pip install setuptools_rust
-python3 -m pip install --upgrade pip
-python3 -m pip install --ignore-installed PyYAML molecule ansible-core
-python3 -m pip install --user "molecule"
-python3 -m pip install --upgrade --user setuptools
+python3 -m pip install setuptools_rust --no-warn-script-location
+python3 -m pip install --upgrade pip --no-warn-script-location
+python3 -m pip install --ignore-installed PyYAML molecule ansible-core --no-warn-script-location
 ansible --version
 python3 -m molecule --version
