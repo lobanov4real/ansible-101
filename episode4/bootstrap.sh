2d880d9572046a22f878ab17e2a5edaba3d17969
lobanov4real <lobanov4real@gmail.com>
2022-11-20 23:52:33 +0300

    add some usability

---
+++ episode4/bootstrap.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo add-apt-repository ppa:deadsnakes/ppa
+apt-get update
+apt-get install -y python3-pip ansible libssl-dev rustc libffi-dev git sshpass fish
+python3 -m pip install setuptools_rust --no-warn-script-location
+python3 -m pip install --upgrade pip --no-warn-script-location
+python3 -m pip install --ignore-installed PyYAML molecule ansible-core --no-warn-script-location
+ansible --version
+python3 -m molecule --version
+git clone https://github.com/lobanov4real/ansible-101
+cd ansible-101/episode4/
+ansible-playbook -i inventory solr.yml
