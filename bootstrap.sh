6154c89ce3abbcfd66988cab2d75fc27403ba760
lobanov4real <lobanov4real@gmail.com>
2022-11-20 22:28:42 +0300

    add ansible Solr installation

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,7 @@
 
 sudo add-apt-repository ppa:deadsnakes/ppa
 apt-get update
-apt-get install -y python3-pip ansible libssl-dev rustc libffi-dev git
+apt-get install -y python3-pip ansible libssl-dev rustc libffi-dev git sshpass
 python3 -m pip install setuptools_rust --no-warn-script-location
 python3 -m pip install --upgrade pip --no-warn-script-location
 python3 -m pip install --ignore-installed PyYAML molecule ansible-core --no-warn-script-location
