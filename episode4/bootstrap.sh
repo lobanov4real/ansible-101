51e6dbb0426bd4155b39d02d5c348c5983e69d0c
lobanov4real <lobanov4real@gmail.com>
2022-11-21 19:30:57 +0300

    edit ansible Solr installation

--- episode4/bootstrap.sh
+++ episode4/bootstrap.sh
@@ -8,7 +8,7 @@ python3 -m pip install --upgrade pip --no-warn-script-location
 python3 -m pip install --ignore-installed PyYAML molecule ansible-core --no-warn-script-location
 ansible --version
 python3 -m molecule --version
-export ANSIBLE_HOST_KEY_CHECKING=False
+sudo export ANSIBLE_HOST_KEY_CHECKING=False
 git clone https://github.com/lobanov4real/ansible-101
 cd ansible-101/episode4/
 ansible-playbook -i inventory solr.yml
