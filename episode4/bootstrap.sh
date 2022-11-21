e35249edd2043a6222cc8dfb2847d5073a8e6c95
lobanov4real <lobanov4real@gmail.com>
2022-11-21 20:32:33 +0300

    edit ansible Solr installation

--- episode4/bootstrap.sh
+++ episode4/bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo add-apt-repository ppa:deadsnakes/ppa
+add-apt-repository ppa:deadsnakes/ppa
 apt-get update
 apt-get install -y python3-pip ansible libssl-dev rustc libffi-dev git sshpass fish
 python3 -m pip install setuptools_rust --no-warn-script-location
@@ -8,7 +8,7 @@ python3 -m pip install --upgrade pip --no-warn-script-location
 python3 -m pip install --ignore-installed PyYAML molecule ansible-core --no-warn-script-location
 ansible --version
 python3 -m molecule --version
-sudo export ANSIBLE_HOST_KEY_CHECKING=False
+export ANSIBLE_HOST_KEY_CHECKING=False
 git clone https://github.com/lobanov4real/ansible-101
 cd ansible-101/episode4/
 ansible-playbook -i inventory solr.yml
