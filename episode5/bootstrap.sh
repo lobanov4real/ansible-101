8e44de4e637790ecb5cbbadb514bbf778c21f5a2
lobanov4real <lobanov4real@gmail.com>
2022-11-22 21:51:14 +0300

    add episode 5

---
+++ episode5/bootstrap.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+add-apt-repository ppa:deadsnakes/ppa
+apt-get update
+apt-get install -y python3-pip ansible libssl-dev rustc libffi-dev git sshpass fish
+python3 -m pip install setuptools_rust --no-warn-script-location
+python3 -m pip install --upgrade pip --no-warn-script-location
+python3 -m pip install --ignore-installed PyYAML molecule ansible-core --no-warn-script-location
+ansible --version
+python3 -m molecule --version
