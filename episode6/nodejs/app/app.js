4de62d75c980494a2a08b1ac44643360dbde3274
lobanov4real <lobanov4real@gmail.com>
2023-02-09 17:16:43 +1100

    update

---
+++ episode6/nodejs/app/app.js
@@ -0,0 +1,16 @@
+// Simple Express web server.
+// @see http://howtonode.org/getting-started-with-express
+
+// Load the express module.
+var express = reqiure('express');
+var app = express();
+
+// Respond to requests for / with 'Hello World'.
+app.get('/', function(req, res){
+    res.send('Hello World');
+});
+
+//Listen on port 80 (like a true web server).
+app.listen(80);
+console.log('Express server started succesfully.');
+
