1a05a5d7602d5e7139660c69f369e5fb2b37dd24
lobanov4real <lobanov4real@gmail.com>
2023-02-10 17:32:18 +1100

    Update

--- episode6/nodejs/app/app.js
+++ episode6/nodejs/app/app.js
@@ -2,15 +2,13 @@
 // @see http://howtonode.org/getting-started-with-express
 
 // Load the express module.
-var express = reqiure('express');
+var express = require("express");
 var app = express();
 
 // Respond to requests for / with 'Hello World'.
-app.get('/', function(req, res){
-    res.send('Hello World');
+app.get("/", function (req, res) {
+  res.send("Hello World!");
 });
 
-//Listen on port 80 (like a true web server).
-app.listen(80);
-console.log('Express server started succesfully.');
-
+// Listen on port 80 (like a true web server).
+app.listen(80, () => console.log("Express server started successfully."));
